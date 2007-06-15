49fe6829a6e0b8831afa88724a898d5b3707c8d0
egal-h <egal-h@dc0a47f8-6732-0410-a3be-f1b0df2a5bc8>
2007-06-15 14:01:11 +0000

    This class is a container for a template in the dotXSI file format, as specified by XSIFTK template reference. It is included in some .XSI models and this makes it possible to load the model although the Renderer does not yet use this information.

---
+++ joXSI/src/com/mojang/joxsi/loader/XSI_ShaderInstanceData.java
@@ -0,0 +1,77 @@
+package com.mojang.joxsi.loader;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * This class is a container for a template in the dotXSI file format, as specified by XSIFTK template reference.
+ * <pre>
+ * XSI_ShaderInstanceData { 
+   <sObjectName>, 
+   <nbParameters>, 
+   <parameter>,
+}
+
+Example
+
+SI_MaterialLibrary MATLIB-tex_test { 
+   1, 
+   XSI_Material DefaultLib.Material { 
+       ...
+       XSI_Shader Image { 
+          "Softimage.txt2d-image-explicit.1", 
+          4, 
+          30, 
+          21, 
+          "Name","STRING","Image",
+          "tspace_id","STRING","Texture_Projection",
+          ...
+          XSI_ShaderInstanceData { 
+              "Model1.sphere",  // Full name of an XSI object
+              1,                   // Number of instance values
+              "tspace_id","STRING","Texture_Projection1", // Instance value
+          }
+       }
+       ...
+   }
+}
+
+ * </pre>
+ * <p>It's very sparsely documented.
+ */
+public class XSI_ShaderInstanceData extends Template
+{
+	public class Parameter implements Serializable
+	{
+		public String name;
+		public String type;
+		public Object value;
+	}
+	
+	public String objectName;
+	public int output;
+	public int param_number;
+	public Parameter[] parameters;
+	public Map parameterMap = new HashMap();
+
+	public void parse(RawTemplate block)
+	{
+		Iterator it = block.values.iterator();
+		
+        objectName = (String)it.next();
+		param_number = ((Integer)it.next()).intValue();
+		
+		parameters = new Parameter[param_number];
+		for (int i=0; i<param_number; i++)
+		{
+			parameters[i] = new Parameter();
+			parameters[i].name = (String)it.next();
+			parameters[i].type = (String)it.next();
+			parameters[i].value = it.next();
+			
+			parameterMap.put(parameters[i].name, parameters[i]);
+		}
+    }
+}
