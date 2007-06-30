84dcaec46cc07dfb29483c68a9c07ed3a1da4df1
egal-h <egal-h@dc0a47f8-6732-0410-a3be-f1b0df2a5bc8>
2007-06-30 14:48:13 +0000

    Added constant ROOT_TEMPLATE = "RootTemplate", which is used in the DotXSILoader when creating a new Root RawTemplate.

--- joXSI/src/com/mojang/joxsi/loader/RawTemplate.java
+++ joXSI/src/com/mojang/joxsi/loader/RawTemplate.java
@@ -11,6 +11,8 @@ class RawTemplate
     public final String name;
     public final String info;
     public final List values = new ArrayList();
+    /** RootTemplate used for creating the root RawTemplate in the DotXSILoader.. */
+    public static final String ROOT_TEMPLATE = "RootTemplate";
 
     public RawTemplate(String name, String info)
     {
