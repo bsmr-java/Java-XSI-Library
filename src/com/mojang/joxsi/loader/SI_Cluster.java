aac6c2e76a4c7f84fe108e88559007e9e0d9d323
discon <discon@dc0a47f8-6732-0410-a3be-f1b0df2a5bc8>
2007-07-19 19:14:08 +0000

    Discon's API breaking quest for glory!

---
+++ src/com/mojang/joxsi/loader/SI_Cluster.java
@@ -0,0 +1,41 @@
+package com.mojang.joxsi.loader;
+
+import java.util.Iterator;
+
+/**
+ * Stores groups of vertices on a model.
+ * This is a sublevel template of the {@link SI_Model } template. 
+ * See also {@link XSI_ClusterInfo }.
+ * This class is a container for a template in the dotXSI file format, as specified by XSIFTK template reference.
+ * 
+ * <p>It's very sparsely documented.
+ * @author Notch
+ * @author Egal
+ */
+public class SI_Cluster extends Template
+{
+	public String referencedModel;
+	public String weighting;
+	public String clusterCenterReference;
+	public int nbVertices;
+	public int[] vertexIndexes;
+
+	public void parse(RawTemplate block) throws ParseException
+	{
+		Iterator<Object> it = block.values.iterator();
+		referencedModel = (String)it.next();
+		weighting = (String)it.next();
+		clusterCenterReference = (String)it.next();
+		
+		nbVertices = ((Integer)it.next()).intValue();
+		vertexIndexes = new int[nbVertices]; 
+		for (int i=0; i<nbVertices; i++)
+		{
+			vertexIndexes[i] = ((Integer)it.next()).intValue();
+		}
+		
+		if(!weighting.equals("AVERAGE") &&
+		   !weighting.equals("ADDITIVE"))
+		    throw new ParseException("Illegal weighting in SI_Cluster: "+weighting);
+	}
+}
---
+++ src/com/mojang/joxsi/loader/XSI_Material.java
@@ -0,0 +1,76 @@
+package com.mojang.joxsi.loader;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+/**
+ * Defines a SOFTIMAGE|XSI material. This template contains the list of 
+ * connections and the name of the sources (if connected).
+ * 
+ * <p>This new material template is closer to the XSI conceptual model. 
+ * In XSI, a material is essentially a placeholder for shaders. 
+ * So now there is an entity that is a placeholder for both realtime and mental 
+ * ray shaders in the dotXSI file format as well.
+ * See also {@link XSI_MaterialInfo }.
+ * 
+ * <p>This class is a container for a template in the dotXSI file format, as specified by XSIFTK template reference.
+ * 
+ * <p>It's very sparsely documented.
+ * @author Notch
+ * @author Egal
+ */
+public class XSI_Material extends Template implements Material
+{
+	public class Connection implements Serializable
+	{
+        /** Name of connection. */
+		public String name;
+        /** Name of connection source. */
+		public String source;
+
+        /**
+         * Returns the name of the connection source.
+         * 
+         * @return the name of the connection source.
+         */
+        public String toString() {
+            return source;
+        }
+	}
+
+    /** Number of connections (that is, <cnx_name, <cnx_source> pairs) listed in this template. */
+	public int cnx_number;
+	public Connection[] connections;
+
+    @Override
+	public void parse(RawTemplate block)
+	{
+		Iterator<Object> it = block.values.iterator();
+		cnx_number = ((Integer)it.next()).intValue();
+		connections = new Connection[cnx_number];
+		for (int i=0; i<cnx_number; i++)
+		{
+			connections[i] = new Connection();
+			connections[i].name = (String)it.next();
+			connections[i].source = (String)it.next();
+		}
+	}
+
+    /**
+     * Returns the clsss name and the number of connections pairs.
+     * 
+     * @return the clsss name and the number of connections pairs.
+     */
+    public String toString()
+    {
+        String lConnectionDetails = "";
+        for (Connection item : connections) {
+            if (item != null && item.toString().length() > 0)
+            {
+                lConnectionDetails += item;
+                lConnectionDetails += ", ";
+            }
+        }
+        return super.toString() + " - cnx_number: " + cnx_number + ", details: " + lConnectionDetails;
+    }
+}
