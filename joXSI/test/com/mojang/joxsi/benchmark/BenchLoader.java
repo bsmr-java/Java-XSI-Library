1ae46a8fdb0df6374946d67efaf8ab4219d716f6
egal-h <egal-h@dc0a47f8-6732-0410-a3be-f1b0df2a5bc8>
2007-07-22 09:39:37 +0000

    benchloader will now load more than one model from the command line. It will report the loading time per model and in total so it can be used to perform tests on a wide range of models at a time.

--- joXSI/test/com/mojang/joxsi/benchmark/BenchLoader.java
+++ joXSI/test/com/mojang/joxsi/benchmark/BenchLoader.java
@@ -8,21 +8,22 @@ import com.mojang.joxsi.loader.DotXSILoader;
 import com.mojang.joxsi.loader.ParseException;
 
 /**
- * Simple benchmarker Parses the model 1,000 times and outputs the speed
- * 
+ * Simple benchmarker Parses the model(s) 1,000 times and outputs the speed
+ *
  * @author Cheater
+ * @author egal
  */
 public class BenchLoader
 {
     /** The number of loops to perform */
-    private static final long tries = 100;
+    private static final long tries = 1000;
 
     private static Logger logger = Logger.getLogger(BenchLoader.class.getName());
     private static ConsoleHandler ch = new ConsoleHandler();
 
     /**
      * Main entry point of the application.
-     * 
+     *
      * @param args
      *            the arguments passed from the commandline
      * @throws IOException
@@ -41,27 +42,49 @@ public class BenchLoader
             // No arguments. We're probably run from webstart, so load the
             // default model
             path = "/DanceMagic.xsi";
+            runTest(path);
         }
         else
         {
-            logger.info("Going to load '" + args[0] + "' as a model");
-            path = "/" + args[0];
+            long overallStartTime = System.currentTimeMillis();
+            // Just the possibility to load more Models at start for better time measurement
+            for (int i = 0; i < args.length; i++)
+            {
+                logger.info("Going to load '" + args[i] + "' as a model");
+                path = "/" + args[i];
+                runTest(path);
+            }
+            long time = System.currentTimeMillis() - overallStartTime;
+            logger.info("It took " + time + " milliseconds to load all the models.");
         }
+        System.exit(0);
+    }
+
+    /**
+     * Run the tests by loading the model from the given path.<br>
+     * Warm up the JVM by loading the model 10 times before running the timed
+     * test.
+     *
+     * @param path
+     *            the path of the model to load
+     * @throws IOException
+     * @throws ParseException
+     */
+    private static void runTest(String path) throws IOException, ParseException
+    {
         // Warming up
         for (int x = 0; x < 10; x++)
         {
             DotXSILoader.load(BenchLoader.class.getResourceAsStream(path));
         }
-
+        logger.info("Finished warming up with model " + path);
         long starttime = System.currentTimeMillis();
         for (int x = 0; x < tries; x++)
         {
             DotXSILoader.load(BenchLoader.class.getResourceAsStream(path));
         }
         long time = System.currentTimeMillis() - starttime;
-        logger.info("It took " + time + " milliseconds to load " + tries + " models.");
-        logger.info((time / tries) + " milliseconds per model.");
-        System.exit(0);
+        logger.info("It took " + time + " milliseconds to load " + path + " as a model " + tries + " models.");
+        logger.info((time / tries) + " milliseconds per model to load " + path);
     }
-
 }
